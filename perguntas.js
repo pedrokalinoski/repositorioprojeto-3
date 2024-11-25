8c7528b8c7cfb9c5b7ae47dc8bd89c4d5464f892
pedrokalinoski <163846978+pedrokalinoski@users.noreply.github.com>
2024-11-25 08:33:10 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O que é mysql?',
+    'Mysql é um gerenciamento de banco de dados'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital do Brasil?',
+    'O capital do brasil é brasilia'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'O que é html?',
+    'É uma linguagem para criar sites'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz nom dia em Inglês?',
+    'bom dia em ingles é Good morning!'
 )
